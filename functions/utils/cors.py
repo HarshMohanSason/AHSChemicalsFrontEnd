942b65027680ec75cd4d887730a9131ef5567385
harshmohansason <harshsason2000@gmail.com>
2025-05-23 16:03:02 +0530

    Accidentally added the .idea folder

--- functions/utils/cors.py
+++
@@ -1,8 +0,0 @@
-from firebase_functions import https_fn
-
-def apply_cors(response: https_fn.Response) -> https_fn.Response:
-    response.headers["Access-Control-Allow-Origin"] = "http://localhost:3000"
-    response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
-    response.headers["Access-Control-Allow-Headers"] = "Content-Type, Authorization"
-    response.headers["Access-Control-Allow-Credentials"] = "true"
-    return response
